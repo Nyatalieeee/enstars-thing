ae69b7d0575618555d29692d345a1b91530b7ca6
Nyatalieeee <nyatalieeee@proton.me>
2024-12-18 21:17:38 -0500

    rework it a little

--- getCharacterNames.js
+++ getCharacterNames.js
@@ -1,6 +1,7 @@
-export default async function getCharNames(charUrl) {
+const url = 'https://tl.data.ensemble.moe/en/characters.json';
+export default async function getCharNames() {
     try {
-        const charData = await fetch(charUrl);
+        const charData = await fetch(url);
         const characters = await charData.json();
         return characters;
     } catch (err) {
--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 import getCharNames from './getCharacterNames.js';
 
-const characters = await getCharNames(
-    'https://tl.data.ensemble.moe/en/characters.json'
-);
+const characters = await getCharNames();
 
 const table = document.getElementById('table');
 const nameButton = document.getElementById('name_button');
+const agreement = document.getElementById('agreement');
 const loading = document.getElementById('loading');
 
 async function generateNames() {
     loading.textContent = 'Loading...';
     table.style.display = 'table';
-    for (let i of characters) {
+    for (let character of characters) {
         const newRow = table.appendChild(document.createElement('tr'));
 
         const charID = newRow.appendChild(document.createElement('td'));
-        charID.textContent = `${i.character_id}`;
+        charID.textContent = `${character.character_id}`;
 
         const charFirstName = newRow.appendChild(document.createElement('td'));
-        charFirstName.textContent = `${i.first_name}`;
+        charFirstName.textContent = `${character.first_name}`;
 
         const charLastName = newRow.appendChild(document.createElement('td'));
-        charLastName.textContent = `${i.last_name}`;
+        if (character.last_name !== undefined)
+            charLastName.textContent = `${character.last_name}`;
     }
     loading.style.display = 'none';
 }
 
 nameButton.onclick = () => {
     generateNames();
+    nameButton.style.display = 'none';
+    agreement.style.display = 'none';
 };
